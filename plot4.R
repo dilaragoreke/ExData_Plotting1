2dbd423b86aec934ff504c9f997d701805c57876
dilaragoreke <dilaragoreke@gmail.com>
2019-01-06 00:55:37 +0300

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -15,14 +15,15 @@ pow_cons$DateTime <- as.POSIXct(paste(as.Date(pow_cons$Date), pow_cons$Time))
 ## Plot 4
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2,2))
-plot(pow_cons$DateTime,pow_cons$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+plot(pow_cons$DateTime,pow_cons$Global_active_power, type="l", ylab="Global Active Power", xlab="")
 
 plot(pow_cons$DateTime,pow_cons$Voltage, type="l", ylab="Voltage", xlab="datetime")
 
 plot(pow_cons$DateTime,pow_cons$Sub_metering_1, type="l",col ="black", ylab="Energy sub metering", xlab="")
 lines(pow_cons$DateTime, pow_cons$Sub_metering_2, col ="red")
 lines(pow_cons$DateTime, pow_cons$Sub_metering_3, col ="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"),lty=1,lwd=1)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"),lty=1,lwd=1,  bty = "n")
 
 plot(pow_cons$DateTime,pow_cons$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
 
